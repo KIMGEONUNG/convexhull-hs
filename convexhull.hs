f6be0bafad8f127c423982ecdf47c662a3d517d1
KIMGEONUNG <saywooong@gmail.com>
2020-08-20 13:45:10 +0900

    feat: instanciate Point2d, Vector2d with Eq, Ord

--- convexhull.hs
+++ convexhull.hs
@@ -1,6 +1,7 @@
+----------------------------------------------------------------
+----BEGIN DEFINITIONS FOR "Point2d"-----------------------------
+
 data Point2d = Point Double Double
-data Vector2d = Vector Double Double
-data Line = Line Point2d Point2d
 
 instance Num Point2d where
     (+) (Point x1 y1) (Point x2 y2) = Point (x1 + x2) (y1 + y2) 
@@ -11,6 +12,41 @@ instance Num Point2d where
     signum (Point x y) = Point (signum x) (signum y) 
     fromInteger v = error "Not Implemented"
 
+instance Eq Point2d where
+    (==) (Point x1 y1) (Point x2 y2) = (x1 == x2) && (y1 == y2) 
+    (/=) (Point x1 y1) (Point x2 y2) = (x1 /= x2) || (y1 /= y2) 
+
+instance Ord Point2d where
+    compare (Point x1 y1) (Point x2 y2) 
+        | (x1 == x2) && (y1 == y2) = EQ
+        | (x1 < x2)                = LT 
+        | (x1 == x2) && (y1 < y2)  = LT 
+        | (x1 > x2)                = GT 
+        | (x1 == x2) && (y1 > y2)  = GT 
+        | otherwise = error "Invalid case"
+    (<) pt1 pt2 = compare pt1 pt2 == LT
+    (<=) pt1 pt2 = a == LT || a == EQ
+                where a = compare pt1 pt2
+    (>) pt1 pt2 = compare pt1 pt2 == GT 
+    (>=) pt1 pt2 = a == GT || a == EQ
+                where a = compare pt1 pt2
+    max pt1 pt2  
+        | pt1 <= pt2 = pt2
+        | otherwise = pt1
+    min pt1 pt2  
+        | pt1 >= pt2 = pt2
+        | otherwise = pt1
+
+instance Show Point2d where
+    show (Point x y) = "X: " ++ show x ++ ", Y: " ++ show y
+
+----END DEFINITIONS FOR "Point2d"-----------------------------
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+----BEGIN DEFINITIONS FOR "Vector2d"--------------------------
+data Vector2d = Vector Double Double
+
 instance Num Vector2d where
     (+) (Vector x1 y1) (Vector x2 y2) = Vector (x1 + x2) (y1 + y2) 
     (-) (Vector x1 y1) (Vector x2 y2) = Vector (x1 - x2) (y1 - y2) 
@@ -20,14 +56,47 @@ instance Num Vector2d where
     signum (Vector x y) = Vector (signum x) (signum y) 
     fromInteger v = error "Not Implemented"
 
-instance Show Point2d where
-    show (Point x y) = "X: " ++ show x ++ ", Y: " ++ show y
+instance Eq Vector2d where
+    (==) (Vector x1 y1) (Vector x2 y2) = (x1 == x2) && (y1 == y2) 
+    (/=) (Vector x1 y1) (Vector x2 y2) = (x1 /= x2) || (y1 /= y2) 
+
+instance Ord Vector2d where
+    compare (Vector x1 y1) (Vector x2 y2) 
+        | (x1 == x2) && (y1 == y2) = EQ
+        | (x1 < x2)                = LT 
+        | (x1 == x2) && (y1 < y2)  = LT 
+        | (x1 > x2)                = GT 
+        | (x1 == x2) && (y1 > y2)  = GT 
+        | otherwise = error "Invalid case"
+    (<) vec1 vec2 = compare vec1 vec2 == LT
+    (<=) vec1 vec2 = a == LT || a == EQ
+                where a = compare vec1 vec2
+    (>) vec1 vec2 = compare vec1 vec2 == GT 
+    (>=) vec1 vec2 = a == GT || a == EQ
+                where a = compare vec1 vec2
+    max vec1 vec2  
+        | vec1 <= vec2 = vec2
+        | otherwise = vec1
+    min vec1 vec2  
+        | vec1 >= vec2 = vec2
+        | otherwise = vec1
 
 instance Show Vector2d where
     show (Vector x y) = "X: " ++ show x ++ ", Y: " ++ show y
 
+----END DEFINITIONS FOR "Vector2d"----------------------------
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+----BEGIN DEFINITIONS FOR "Line"------------------------------
+
+data Line = Line Point2d Point2d
+
 instance Show Line where
     show (Line pt1 pt2) = "FROM: (" ++ show pt1 ++ "), TO: (" ++ show pt2 ++ ")" 
+
+----END DEFINITIONS FOR "Line"--------------------------------
+--------------------------------------------------------------
 
 -- return radian value
 vectorAngle :: Vector2d -> Vector2d -> Double
@@ -48,5 +117,18 @@ radianToDegree r = 180.0 / pi * r
 degreeToRadian :: Double -> Double
 degreeToRadian d = pi / 180.0 * d  
 
+quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser  = filter (< p) xs
+        greater = filter (>= p) xs
+
+--giftWrapping :: [Point2d] -> [Point2d]
+
 v1 = Vector 10 0
 v2 = Vector 0 10
+
+pt1 = Point 10 0
+pt2 = Point 0 10
+pts = [ Point 1 0, Point 2 1, Point 2 0, Point (-1) 3]
