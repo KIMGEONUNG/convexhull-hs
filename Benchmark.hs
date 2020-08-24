4f0c42c3434b4959e02b8a01f9bf70ed2c1c8185
KIMGEONUNG <saywooong@gmail.com>
2020-08-24 20:46:22 +0900

    feat: implement monad version for Benchmark

--- Benchmark.hs
+++ Benchmark.hs
@@ -8,7 +8,7 @@ import Data.Typeable
 import Convexhull
 import Geos
 
-takeNum= 10000
+takeNum= 1000
 
 data Entry = Entry { x :: Double, y:: Double} deriving Show
 parseEntry (a:b:_) = Entry (read a :: Double) (read b :: Double)
@@ -16,9 +16,22 @@ parseEntry (a:b:_) = Entry (read a :: Double) (read b :: Double)
 entry2point :: Entry -> Point2d
 entry2point (Entry x y) = Point x y 
 
-pts = map entry2point . map parseEntry . map (take 2) . map (drop 1) . map words . take takeNum . drop 1 . lines <$> readFile "N10000.txt"
+parser = map entry2point . map parseEntry . map (take 2) . map (drop 1) . map words . take takeNum . drop 1 . lines  
 
-main = do
-    convexPts <- giftWrapping <$> pts 
-    putStrLn "Convexhull Points"
-    print convexPts 
+main = 
+    parser <$> readFile "N10000.txt" 
+    >>= \pts -> giftWrapping <$> return pts 
+    >>= \convexPts -> print convexPts
+
+
+{-
+main = parser                <$> readFile "N10000.txt" >>=  \pts -> giftWrapping                    <$> pure pts       >>=   \convexPts -> print convexPts
+     = {String -> [Point2d]} <$> {IO String}           >>= (\pts -> {[Point2d] -> [Point2d]}        <$> pure pts       >>=  (\convexPts -> {IO ()}))
+     = {IO [Point2d]}                                  >>= (\pts -> {[Point2d] -> [Point2d]}        <$> pure pts       >>=  (\convexPts -> {IO ()}))
+     = {IO [Point2d]}                                  >>= ({[Point2d]} -> {[Point2d] -> [Point2d]} <$> {IO [Point2d]} >>= ({[Point2d]} -> {IO ()}))
+     = {IO [Point2d]}                                                                                                  >>= ({[Point2d]} -> {IO ()})
+     =                                                                                                                                     {IO ()}
+
+*same with main = parser <$> readFile "N10000.txt" >>= \pts -> giftWrapping <$> pure pts >>= \convexPts -> print convexPts
+-} 
+
